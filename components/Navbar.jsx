36a4c95636fd06c4a83246e5224caf21b9fa506b
Dhruv Gajjar <100516908+Dhruv-Gajjar@users.noreply.github.com>
2022-05-23 21:13:20 +0530

    added hero component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NextLink from "next/link";
 import logo from "../public/assets/logo.png";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { FaHome } from "react-icons/fa";
 
 const Navbar = () => {
   const [toggleNav, setToggleNav] = useState(false);
@@ -12,7 +13,8 @@ const Navbar = () => {
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex items-center justify-between py-4">
           <NextLink href="/">
-            <a>
+            <a className="flex items-center space-x-2">
+              <FaHome size={20} />
               <h2 className="text-2xl font-bold">Design.</h2>
             </a>
           </NextLink>
@@ -50,13 +52,14 @@ const Navbar = () => {
           <div
             className={
               !toggleNav
-                ? "fixed md:hidden items-center justify-between left-0 top-0 bg-[#1e4344]  shadow-xl w-[60%] h-full ease-in-out duration-300"
+                ? "fixed z-[100] md:hidden items-center justify-between left-0 top-0 bg-[#1e4344] shadow-xl w-[60%] h-full ease-in-out duration-500"
                 : "fixed hidden"
             }
           >
             <div className="text-center m-8 pt-10 space-y-8 text-[#e8f1f1]">
               <NextLink href="/">
-                <a>
+                <a className="flex items-center justify-center space-x-2">
+                  <FaHome size={20} />
                   <h2 className="text-2xl font-bold">Design.</h2>
                 </a>
               </NextLink>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Design.</h1>
+    <div className="w-full h-[80vh] relative text-[#e8f1f1]">
+      <div className="absolute bg-gradient-to-t from-black/80 left-0 top-0 w-full h-full z-30"></div>
+      <div className="w-full h-full absolute left-0 top-0 flex flex-col items-center justify-center space-y-5 capitalize p-6 z-30">
+        <h1 className="text-5xl md:text-6xl xl:text-7xl font-bold">
+          Your imagination our creation
+        </h1>
+        <p className="text-xl md:text-2xl xl:text-3xl">
+          Everyone can achive creativity
+        </p>
+      </div>
+
+      <Image
+        src="/../public/assets/hero.jpg"
+        alt="hero image"
+        layout="fill"
+        className="object-cover"
+      />
     </div>
-  )
+  );
 }
