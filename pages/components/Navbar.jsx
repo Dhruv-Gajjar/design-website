d1bab295eab82830b9a3a1e88520c38c9fe14a88
Dhruv Gajjar <100516908+Dhruv-Gajjar@users.noreply.github.com>
2022-05-23 11:09:06 +0530

    navbar updated

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,22 +1,21 @@
-import {useState} from "react";
+import { useState } from "react";
 import NextLink from "next/link";
-import { AiOutlineMenu } from "react-icons/ai";
+import logo from "../../public/assets/logo.png";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
-  const [toggleNav, setToggleNav] = useState(false)
+  const [toggleNav, setToggleNav] = useState(false);
 
   return (
-    <div className="bg-teal-700 text-zinc-100">
+    <div className="bg-gray-100 text-black shadow-lg text-gray-700">
       {/* Desktop nav */}
-      <div className=" mx-auto px-8">
+      <div className="max-w-6xl mx-auto px-4">
         <div className="flex items-center justify-between py-4">
-          <div>
-            <NextLink href="/">
-              <a>
-                <h4 className="text-2xl font-bold">Design</h4>
-              </a>
-            </NextLink>
-          </div>
+          <NextLink href="/">
+            <a>
+              <h2 className="text-2xl font-bold">Design.</h2>
+            </a>
+          </NextLink>
 
           <div className="hidden md:flex space-x-3 ">
             <NextLink href="/services">
@@ -36,15 +35,44 @@ const Navbar = () => {
               Signin
             </button>
           </div>
-
           {/* Mobile nav */}
-          <div className="inline-block md:hidden">
-              <button onClick={() => setToggleNav(!toggleNav)}>
-                <i>
-                  <AiOutlineMenu size={20} />
-                </i>
-              </button>
+          <div className="block md:hidden">
+            <button onClick={() => setToggleNav(!toggleNav)}>
+              {!toggleNav ? (
+                <AiOutlineClose size={20} />
+              ) : (
+                <AiOutlineMenu size={20} />
+              )}
+            </button>
+          </div>
+
+          <div
+            className={
+              !toggleNav
+                ? "fixed md:hidden items-center justify-between left-0 top-0 bg-gray-100 shadow-xl w-[60%] h-full transition-all ease-in-out duration-500"
+                : "fixed hidden"
+            }
+          >
+            <div className="text-center m-8 pt-10 space-y-8 text-gray-700">
+              <NextLink href="/">
+                <a>
+                  <h2 className="text-2xl font-bold">Design.</h2>
+                </a>
+              </NextLink>
+
+              <div className="flex flex-col text-center md:hidden space-y-5">
+                <NextLink href="/services">
+                  <a className="border-b border-gray-300">Services</a>
+                </NextLink>
+                <NextLink href="/about">
+                  <a className="border-b border-gray-300">About</a>
+                </NextLink>
+                <NextLink href="/contact">
+                  <a>Contact</a>
+                </NextLink>
+              </div>
             </div>
+          </div>
         </div>
       </div>
     </div>
