1c9abc3427b345f4656e438e67d437bf2ab9a700
Dhruv Gajjar <100516908+Dhruv-Gajjar@users.noreply.github.com>
2022-05-25 12:00:29 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import img1 from "../public/assets/img1.jpg";
 
 export default function Home() {
   return (
@@ -23,7 +24,7 @@ export default function Home() {
         />
       </div>
 
-      <div className="w-full h-full grid grid-cols-3 gap-4 items-center justify-center bg-[#1e4344] text-[#e8f1f1] p-[3rem]">
+      <div className="w-full h-full grid grid-cols-1 md:grid-cols-3 gap-4 items-center justify-center bg-[#1e4344] text-[#e8f1f1] p-[3rem]">
         <div className="text-center space-y-3">
           <h4 className="capitalize">client satisfaction</h4>
           <p className="font-bold text-xl">100%</p>
@@ -40,22 +41,20 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="bg-white py-5 grid grid-cols-2 gap-8 w-full h-full items-start justify-between px-8 py-6">
-        <Image
-          src="/../public/assets/img1.jpg"
-          width="1000"
-          height="500"
-          className="object-cover h-full"
-        />
+      <div className="bg-white py-5 grid grid-cols-1 md:grid-cols-2 gap-6 w-full h-fit items-start justify-between px-8 py-6">
+        <div className="w-full h-[400px] relative">
+          <Image src={img1} layout="fill" className="object-cover" />
+        </div>
+
         <div className="space-y-12 p-5 w-full h-full flex items-start flex-col justify-start">
-          <h1 className="capitalize text-4xl text-center font-bold">
+          <h1 className="capitalize text-4xl xl:text-5xl text-center font-bold">
             how we serve
           </h1>
-          <p className="capitalize text-2xl text-gray-800">
+          <p className="capitalize text-2xl xl:text-2xl text-gray-800">
             true value of the brand is & always will be human capital
           </p>
 
-          <p className="text-lg text-gray-500">
+          <p className="text-lg xl:text-xl text-gray-500">
             Therefore we assemble our team with top talents,who are passionate
             about what we do, with a clear vision and a spectacular attitude to
             the creation process.
